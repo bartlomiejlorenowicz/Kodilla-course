fe50db230d2f5e29a1c4deeb22488829b025df33
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2025-11-11 14:44:50 +0100

    Modul 6.2 Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Modul  6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK!");
+        } else  {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(1, 2);
+        int subtractResult = calculator.subtract(4, 2);
+
+        if (addResult == 3) {
+            System.out.println("test OK!");
+        } else  {
+            System.out.println("Error!");
+        }
+
+        if (subtractResult == 2) {
+            System.out.println("test OK!");
+        } else  {
+            System.out.println("Error!");
+        }
     }
 }
