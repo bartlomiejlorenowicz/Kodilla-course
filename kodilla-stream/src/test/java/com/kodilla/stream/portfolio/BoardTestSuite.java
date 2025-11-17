dfbca2aa1f8644c46735891cc8cbb511ca5b31fe
Bartlomiej <bartlomiejlorenowicz@gmail.com>
2025-11-17 16:37:02 +0100

    Add Module 7.4: test: add unit test for average days of tasks in 'In progress' list

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -142,4 +142,24 @@ public class BoardTestSuite {
         //Then
         assertEquals(2, longTasks);
     }
+
+    @Test
+    void testAddTaskListAverageWorkingOnTask() {
+        //Given
+        Board project = prepareTestData();
+
+        //When
+        List<TaskList> inProgressTasks = new ArrayList<>();
+        inProgressTasks.add(new TaskList("In progress"));
+        double averageDays = project.getTaskLists().stream()
+                .filter(inProgressTasks::contains)
+                .flatMap(tl -> tl.getTasks().stream())
+                .map(Task::getCreated)
+                .mapToLong(created -> LocalDate.now().toEpochDay() - created.toEpochDay())
+                .average()
+                .orElse(0);
+
+        //Then
+        assertEquals(10.0, averageDays);
+    }
 }
