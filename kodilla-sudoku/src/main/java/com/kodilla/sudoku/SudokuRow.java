5b02d85df7297709ae5d96c125ab788dfe90978d
Bartlomiej <bartlomiejlorenowicz@gmail.com>
2026-01-24 14:45:04 +0100

    Add elements initialization to SudokuRow class

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuRow.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuRow.java
@@ -1,4 +1,19 @@
 package com.kodilla.sudoku;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SudokuRow {
+    private List<SudokuElement> elements = new ArrayList<>();
+
+    public SudokuRow() {
+        for (int i = 0; i < 9; i++) {
+            elements.add(new SudokuElement());
+        }
+    }
+
+    public List<SudokuElement> getElements() {
+        return elements;
+    }
 }
+
