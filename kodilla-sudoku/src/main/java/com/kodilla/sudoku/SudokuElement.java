f0b6bab382b3c55d2e528b21e76cb002f1fd75a2
Bartlomiej <bartlomiejlorenowicz@gmail.com>
2026-01-24 14:45:29 +0100

    Add SudokuElement class with value and possible values initialization

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuElement.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuElement.java
@@ -1,4 +1,23 @@
 package com.kodilla.sudoku;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SudokuElement {
+    public static final int EMPTY = -1;
+
+    private int value;
+    private List<Integer> possibleValues = new ArrayList<>();
+
+    public SudokuElement() {
+        this.value = EMPTY;
+        for (int i = 1; i <= 9; i++) {
+            possibleValues.add(i);
+        }
+    }
+
+    public int getValue() { return value; }
+    public void setValue(int value) { this.value = value; }
+
+    public List<Integer> getPossibleValues() { return possibleValues; }
 }
